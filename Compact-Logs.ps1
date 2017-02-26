dd072de66681e914e8259800d0d3dd3903630710
Ryan Bemrose <pi@bemrose.us>
2017-02-25 23:29:13 -0800

    Compact-Logs.ps1 - Turn daily icechat logfiles into monthly

---
+++ Compact-Logs.ps1
@@ -0,0 +1,29 @@
+﻿# IceChat log compacter
+# 
+# Usage:
+# CD to logs directory
+#   .\Compact-Logs.ps1 NoAgenda 2016 5
+#
+# batch whole year:
+#   1..12 | % { .\Compact-Logs.ps1 NoAgenda 2016 $_ }
+#
+#
+# Assumes logs are named daily with '#Channel-yyyy-MM-dd.log'
+# Creates monthly log named 'Channel-yyyy-MM.log'
+# Individual date is prepended to each line of log so that day isn't lost
+
+
+param([string]$channel, [int]$year, [int]$month)
+
+$prefix = '.\#{0}-{1}-{2:00}-*' -f $channel, $year, $month
+$outfile = '.\{0}-{1}-{2:00}.log' -f $channel, $year, $month
+
+gci $prefix | foreach {
+
+    $prepend =  if ($_ -match '(\d{4}-\d\d-\d{2})') { "$($matches[1]) " }
+
+    Get-Content $_.FullName | foreach { 
+        $prepend + $_
+    } | Out-File $outfile -Append -Encoding utf8
+
+}
